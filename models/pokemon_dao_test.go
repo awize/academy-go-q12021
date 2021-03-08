ab637d49bd1ebc40e670e8354d5cac2ee429d34c
Alexis Jimenez <alexis.jimenez@wizeline.com>
2021-03-07 20:56:32 -0600

    feat: add test when pokemon is not found

--- models/pokemon_dao_test.go
+++ models/pokemon_dao_test.go
@@ -17,3 +17,15 @@ func TestGetPokemon(t *testing.T) {
 		t.Error("we did not expect any error")
 	}
 }
+
+func TestGetPokemonNotFound(t *testing.T) {
+	pokemon, err := GetPokemon(0)
+	emptyPokemon := Pokemon{}
+	if pokemon != emptyPokemon {
+		t.Error("expected a empty pokemon")
+	}
+
+	if err == nil {
+		t.Error("we did expect an error")
+	}
+}
